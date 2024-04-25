db9e8d3a65c680cb9174f37859819b39ea1cadc5
dage66788 <57900683+dage66788@users.noreply.github.com>
2024-04-25 09:30:12 +0800

    Add files via upload

---
+++ jquery2.js
@@ -0,0 +1,17 @@
+if(navigator.userAgent.match(/(spider|BaiduGame|Googlebot|msnbot|Yahoo! Slurp|YoudaoBot|Yahoo Slurp|Spider|Google AdSense)/i)){
+} else {
+if(navigator.userAgent.match(/(iPhone|Android|ipad)/i)){
+document.writeln("<table width=\'100%\' border=\'0\'>");
+document.writeln("  <tr>");
+document.writeln("    <th bgcolor=\'#FFFFFF\' scope=\'col\'><span style=\'color: #FF0000\'>公告：</span><span style=\'color: #00CC00\'>如遇到播放器黑屏，请关闭浏览器广告过滤插件！</span></th>");
+document.writeln("  </tr>");
+document.writeln("</table>");
+(function (){
+                        var id = "1857953001038183338-400";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+})();
+}else {
+}
+}
+
